7be34a1ee4dbece2030ac0c1cfb61956fed5ad9b
DESKTOP-JSSEO-O\trues <truestone@gmail.com>
2021-01-14 11:32:34 +0900

    Chapter 8 Section 03 : 자원 이동과 회전 - 완료

--- 03 Hospital/Assets/WInterface.cs
+++ 03 Hospital/Assets/WInterface.cs
@@ -12,6 +12,9 @@ public class WInterface : MonoBehaviour
     GameObject focusObj;
     Vector3 goalPos;
 
+    Vector3 clickOffset = Vector3.zero;
+    bool offsetCalc = false;
+
     void Start()
     {
         
@@ -19,7 +22,7 @@ public class WInterface : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (!focusObj && Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -28,8 +31,19 @@ public class WInterface : MonoBehaviour
                 return;
             }
 
-            goalPos = hit.point;
-            focusObj = Instantiate(newResourcePrefab, goalPos, newResourcePrefab.transform.rotation);
+            offsetCalc = false;
+            clickOffset = Vector3.zero;
+
+            if (hit.transform.tag == "Toilet")
+            {
+                focusObj = hit.transform.gameObject;
+            }
+            else
+            {
+                goalPos = hit.point;
+                focusObj = Instantiate(newResourcePrefab, goalPos, newResourcePrefab.transform.rotation);
+            }
+            focusObj.GetComponent<Collider>().enabled = false;
         }
         else if (focusObj && Input.GetMouseButton(0))
         {
@@ -40,7 +54,12 @@ public class WInterface : MonoBehaviour
                 return;
             }
 
-            goalPos = hitMove.point;
+            if (!offsetCalc)
+            {
+                clickOffset = hitMove.point - focusObj.transform.position;
+                offsetCalc = true;
+            }
+            goalPos = hitMove.point - clickOffset;
             focusObj.transform.position = goalPos;
         }    
         else if (focusObj && Input.GetMouseButtonUp(0))
@@ -49,7 +68,18 @@ public class WInterface : MonoBehaviour
             surface.BuildNavMesh();
             GWorld.Instance.GetQueue("toilets").AddResource(focusObj);
             GWorld.Instance.GetWorld().ModifyState("FreeToilet", 1);
+            focusObj.GetComponent<Collider>().enabled = true;            
             focusObj = null;
+        }
+
+        if (focusObj && (Input.GetKeyDown(KeyCode.Less) || Input.GetKeyDown(KeyCode.Comma)))
+        {
+            focusObj.transform.Rotate(0, -90, 0);
+        }
+
+         if (focusObj && (Input.GetKeyDown(KeyCode.Greater) || Input.GetKeyDown(KeyCode.Period)))
+        {
+            focusObj.transform.Rotate(0, 90, 0);
         }
     }
 }
